052d44f83ac1ac78f86db356dbbc0569a94a79f1
Ümit Akköse <umieat@gmail.com>
2022-09-09 20:26:00 +0300

    Update flamingo.construct_flamingo_from_hic.R

--- FLAMINGOm/R/flamingo.construct_flamingo_from_hic.R
+++ FLAMINGOm/R/flamingo.construct_flamingo_from_hic.R
@@ -14,7 +14,7 @@
 flamingo.construct_flamingo_from_hic <- function(hic_file,normalization,resolution,chr_name,chr_size,alpha=-0.25,n_row=45000){
   options(scipen = 999)
 #  chr_number <- gsub("chr","",chr_name)
-  normalized_data = strawr::straw(normalization,hic_file,chr_number,chr_number,unit='BP',binsize=resolution)
+  normalized_data = strawr::straw(normalization,hic_file,chr_name,chr_name,unit='BP',binsize=resolution)
   n <- ceiling(chr_size/resolution)
   i_ind <- 1+(normalized_data[,1]/resolution)
   j_ind <- 1+(normalized_data[,2]/resolution)
